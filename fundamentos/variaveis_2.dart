ce4662414f0ec7b45f44fbeaf485bb80e0401ef0
Logon Aluno <logonrmlocal@fiap.com.br>
2024-02-26 22:12:07 -0300

    Dart - fundamentos

---
+++ fundamentos/variaveis_2.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  var n1 = 2;
+  var n2 = 4.56;
+  var n3 = 'O valor da soma é: ';
+  var n4 = true;
+
+  print(n1.runtimeType);
+  print(n2.runtimeType);
+  print(n3.runtimeType);
+  print(n4.runtimeType);
+}
