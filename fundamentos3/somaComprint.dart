790b40f60bb2faaffb2c6f6e665c4a70853165fe
kenzotanaka17 <andre.kenzo176@gmail.com>
2024-04-01 19:55:21 -0300

    refact: Refatoração do arquivo somaComprint.dart

--- fundamentos3/somaComprint.dart
+++ fundamentos3/somaComprint.dart
@@ -1,16 +1,18 @@
+import 'dart:math';
+
 void main(List<String> args) {
-  int a = 2;
-  int b = 3;
-
-  print (a + b);
-
-  int c = 4;
-  int d = 5;
-
-  print(c + d);
-  SOMACOMPRINT();
+  somaPrint(2, 3);
+  somaPrint(4, 5);
+  somaNumerosAleatorios();
 }
 
-SOMACOMPRINT() {
-  print("Execute");
+void somaPrint(int a, int b) {
+  print(a + b);
+}
+
+void somaNumerosAleatorios() {
+  int n1 = Random().nextInt(11);
+  int n2 = Random().nextInt(11);
+  print("Os valores sorteados foram $n1 e $n2");
+  print(n1 + n2);
 }
