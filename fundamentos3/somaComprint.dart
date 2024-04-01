96c8240d9551eb95912d2e512f90f0b64befae48
kenzotanaka17 <andre.kenzo176@gmail.com>
2024-04-01 19:52:07 -0300

    feat: Adicionando arquivo somaComprint.dart

---
+++ fundamentos3/somaComprint.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  int a = 2;
+  int b = 3;
+
+  print (a + b);
+
+  int c = 4;
+  int d = 5;
+
+  print(c + d);
+  SOMACOMPRINT();
+}
+
+SOMACOMPRINT() {
+  print("Execute");
+}
