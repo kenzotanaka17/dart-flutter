85bb62eff9efc3c10f2ebafb93e192b46bf39b99
kenzotanaka17 <andre.kenzo176@gmail.com>
2024-04-08 20:36:04 -0300

    feat: map_reduce3

---
+++ funcoes/map_reduce3.dart
@@ -0,0 +1,24 @@
+void main(List<String> args) {
+  var alunos = [
+    {
+      'nome': 'Alfredo', 'nota': 9.9
+    },
+    {
+      'nome': 'Wilson', 'nota': 9.3
+    },
+    {
+      'nome': 'Mariana', 'nota': 8.7
+    },
+    {
+      'nome': 'Ana', 'nota': 7.6
+    },
+    {
+      'nome': 'Ricardo', 'nota': 6.8
+    }
+  ];
+  var total = alunos
+  .map( (aluno) => aluno['nota'] )
+  .map( (nota) => (nota as double) )
+  .reduce( (t, a) => t + a );
+  print(total);
+}
